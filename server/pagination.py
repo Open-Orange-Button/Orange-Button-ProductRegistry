5e2d962338b672fa93e1bc05158458a623d2f29d
Taos Transue <spacechips@rocketmail.com>
2023-03-25 17:20:18 -0600

    adding pagination over SubstituteProducts, no sql queries when object is missing fields

--- server/pagination.py
+++ server/pagination.py
@@ -1,19 +1,92 @@
 from collections import OrderedDict
 
 from django import conf
-from rest_framework import response
-from rest_framework import pagination
+from rest_framework import response, pagination, utils
 
 
 class ProductsPagination(pagination.LimitOffsetPagination):
     limit_query_param = 'limit_Products'
     offset_query_param = 'offset_Products'
+    limit_SubstituteProducts_query_param = 'limit_SubstituteProducts'
+    offset_SubstituteProducts_query_param = 'offset_SubstituteProducts'
     max_limit = conf.settings.REST_FRAMEWORK['MAX_LIMIT']
 
-    def get_paginated_response(self, data):
-        return response.Response(OrderedDict([
-            ('count', self.count),
-            ('next', self.get_next_link()),
-            ('previous', self.get_previous_link()),
-            ('Products', data)
-        ]))
+    def paginate_queryset(self, queryset, request, view=None, max_count_SubstituteProducts=None):
+        page = super().paginate_queryset(queryset, request, view=view)
+
+        self.max_count_SubstituteProducts = max_count_SubstituteProducts
+        self.offset_SubstituteProducts = self.get_offset_SubstituteProducts(request)
+        self.limit_SubstituteProducts = self.get_limit_SubstituteProducts(request)
+
+        return page
+
+    def get_offset_SubstituteProducts(self, request):
+        try:
+            offset = int(request.query_params[self.offset_SubstituteProducts_query_param])
+            if offset <= 0:
+                return 0
+            return offset
+        except (KeyError, ValueError):
+            return 0
+
+    def get_limit_SubstituteProducts(self, request):
+        try:
+            limit = int(request.query_params[self.limit_SubstituteProducts_query_param])
+            if limit <= 0:
+                return self.default_limit
+            return limit
+        except (KeyError, ValueError):
+            return self.default_limit
+
+    def get_next_Products_link(self):
+        return super().get_next_link()
+
+    def get_prev_Products_link(self):
+        return super().get_previous_link()
+
+    def get_next_SubstituteProducts_link(self):
+        offset_SubstituteProducts = self.offset_SubstituteProducts + self.limit_SubstituteProducts
+        if offset_SubstituteProducts >= self.max_count_SubstituteProducts:
+            return None
+
+        url = self.request.build_absolute_uri()
+
+        url = utils.urls.replace_query_param(url, self.limit_SubstituteProducts_query_param, self.limit_SubstituteProducts)
+        return utils.urls.replace_query_param(url, self.offset_SubstituteProducts_query_param, offset_SubstituteProducts)
+
+    def get_prev_SubstituteProducts_link(self):
+        if self.offset_SubstituteProducts <= 0:
+            return None
+
+        url = self.request.build_absolute_uri()
+
+        url = utils.urls.replace_query_param(url, self.limit_SubstituteProducts_query_param, self.limit_SubstituteProducts)
+
+        offset_SubstituteProducts = self.offset_SubstituteProducts - self.limit_SubstituteProducts
+        if offset_SubstituteProducts <= 0:
+            return utils.urls.remove_query_param(url, self.offset_SubstituteProducts_query_param)
+
+        return utils.urls.replace_query_param(url, self.offset_SubstituteProducts_query_param, offset_SubstituteProducts)
+
+    def get_paginated_response(self, data, debug_dict=None):
+        if self.max_count_SubstituteProducts is None:
+            res = OrderedDict(
+                count_Products=self.count,
+                next_Products=self.get_next_Products_link(),
+                prev_Products=self.get_prev_Products_link(),
+                Products=data
+            )
+        else:
+            res = OrderedDict(
+                count_Products=self.count,
+                next_Products=self.get_next_Products_link(),
+                prev_Products=self.get_prev_Products_link(),
+                max_count_SubstituteProducts=self.max_count_SubstituteProducts,
+                next_SubstituteProducts=self.get_next_SubstituteProducts_link(),
+                prev_SubstituteProducts=self.get_prev_SubstituteProducts_link(),
+                Products=data
+            )
+        if self.request.query_params.get('debug', None) == 'true':
+            res['debug'] = debug_dict
+            res.move_to_end('debug', last=False)
+        return response.Response(res)
--- server/views_api.py
+++ server/views_api.py
@@ -12,11 +12,11 @@ PARAM_LIMIT_SUBSTITUTEPRODUCTS = 'limit_SubstituteProducts'
 
 
 def _SELECT(relation, join_col, has_group_by: bool, window_name='query_group'):
-    columns = [f'{relation}.{join_col}']
+    columns = [f'{relation}.{join_col} as id']
     if has_group_by:
         columns += [
             f'min({relation}.{join_col}) OVER {window_name} as first',
-            f'ROW_NUMBER() OVER {window_name} as group_row'
+            f'ROW_NUMBER() OVER {window_name} as group_row_num'
         ]
     return f"SELECT {', '.join(columns)} "
 
@@ -49,7 +49,8 @@ def _get_WHERE_GROUP_BY_clauses(info_dict):
         for m_info in m_info_list:
             relation = m_info['relation']
             fields = m_info['fields']
-            for f, v in fields:
+            for f_dict in fields:
+                f, v = f_dict['col'], f_dict['val']
                 if v == '':
                     group_bys.append(f'{relation}.{f}')
                 else:
@@ -67,8 +68,8 @@ def _WINDOW_PARTITION_BY(relation, join_col, group_by_clauses, window_name='quer
     return f"WINDOW {window_name} as (PARTITION BY {', '.join(group_by_clauses)} ORDER BY {relation}.{join_col})"
 
 
-def _PaginationSubstitueProducts(query_relation, row_num_col='group_row'):
-    return f'WHERE ({query_relation}.{row_num_col}=1 OR {query_relation}.{row_num_col}>=%({PARAM_OFFSET_SUBSTITUTEPRODUCTS})s AND {query_relation}.{row_num_col}<%({PARAM_LIMIT_SUBSTITUTEPRODUCTS})s) '
+def _PaginationSubstitueProducts(query_relation, row_num_col='group_row_num'):
+    return f'WHERE ({query_relation}.{row_num_col}=1 OR {query_relation}.{row_num_col}>=%({PARAM_OFFSET_SUBSTITUTEPRODUCTS})s AND {query_relation}.{row_num_col}<%({PARAM_LIMIT_SUBSTITUTEPRODUCTS})s)'
 
 
 def _build_subquery(info_dict):
@@ -81,25 +82,31 @@ def _build_subquery(info_dict):
         WHERE = _WHERE(wheres)
     subquery = f'{SELECT}{FROM_TABLES}{WHERE}'
     WINDOW_PARTITION_BY = ''
-    PaginationSubstitueProducts = ''
+    paginationSubstitueProducts = ''
     if has_group_by:
         WINDOW_PARTITION_BY = _WINDOW_PARTITION_BY(relation_first, join_col_first, group_bys)
-        query_groups_relation = 'query_group'
-        PaginationSubstitueProducts = _PaginationSubstitueProducts(query_groups_relation)
-        subquery = f'SELECT id, first FROM ({subquery}{WINDOW_PARTITION_BY}) as {query_groups_relation} {PaginationSubstitueProducts}'
-    print(subquery)
+        query_groups_relation = 'query_group_data'
+        paginationSubstitueProducts = _PaginationSubstitueProducts(query_groups_relation)
+        subquery = f'SELECT id, first, group_size FROM (SELECT id, first, group_row_num, COUNT(id) OVER (PARTITION BY first) as group_size FROM ({subquery}{WINDOW_PARTITION_BY}) as query_group) as {query_groups_relation} {paginationSubstitueProducts}'
     return subquery, params, has_group_by
 
 
 def build_subquery_info(query_data, info_dict, name, parent_name):
+    if info_dict['!missing_fields'] != '':
+        return  # keys are missing so abort
+
     superclass = obit.get_schema_superclass(name)
     if superclass is not None:
         # ensure the superclass table can be joined on
         build_subquery_info(query_data, info_dict, superclass, None)
+        info_dict['!missing_fields'] = ''  # the superclass might not have all fields
 
     # bucket the fields declared on the OB definition, excluding superclass fields
     elements, objects, arrays = [], [], []
-    for f in set(obit.all_fields_of_ob_object(name, include_inherited=False)).intersection(set(query_data.keys())):
+    fields_query_data = set(query_data.keys())
+    if len((missing_fields := fields_query_data - set(obit.all_fields_of_ob_object(name, include_inherited=True)))) > 0:
+        info_dict['!missing_fields'] = ', '.join(sorted(missing_fields))  # flag keys are missing
+    for f in set(obit.all_fields_of_ob_object(name, include_inherited=False)) & fields_query_data:
         match obit.get_schema_type(f):
             case obit.OBType.Element:
                 elements.append(f)
@@ -108,7 +115,7 @@ def build_subquery_info(query_data, info_dict, name, parent_name):
             case obit.OBType.Array:
                 arrays.append(f)
 
-    # create the query info about this model of the query_data
+    # create the query info about the model corresponding to query_data
     if name not in info_dict:
         info_dict[name] = []
     parent_model_name = superclass or parent_name or None
@@ -133,7 +140,7 @@ def build_subquery_info(query_data, info_dict, name, parent_name):
     for f in elements:
         for p in obit.Primitive:
             if (v := query_data[f].get(p.name, None)) is not None:
-                m_info['fields'].append((f'{f}_{p.name}', v))
+                m_info['fields'].append(dict(col=f'{f}_{p.name}', val=v))
 
     info_dict[name].append(m_info)  # leave out if fields length is zero?
 
@@ -148,10 +155,23 @@ def build_subquery_info(query_data, info_dict, name, parent_name):
 
 def build_query(prod_types, request):
     subqueries = []
+    info_dicts = []
     for pt in prod_types:
-        info_dict = OrderedDict()
+        info_dict = OrderedDict([('!missing_fields', '')])
         build_subquery_info(request.data, info_dict, pt, None)
+        info_dicts.append(info_dict)
+        if info_dict['!missing_fields'] != '':
+            continue  # no queries on objects missing query fields
+        del info_dict['!missing_fields']
         subqueries.append(_build_subquery(info_dict))
+    debug_dict = dict(
+        query='',
+        query_params={},
+        has_group_by=None,
+        subquery_intermediate_representations=info_dicts
+    )
+    if len(subqueries) == 0:
+        return None, None, None, debug_dict
     subquery_strings = []
     params = dict()
     has_group_by = False
@@ -168,7 +188,14 @@ def build_query(prod_types, request):
         params[PARAM_OFFSET_SUBSTITUTEPRODUCTS] = current_offset + 2  # row_number starts counting at 1, and the first row is the first product. Therefore, the first SubstituteProduct is at row_number 2
         params[PARAM_LIMIT_SUBSTITUTEPRODUCTS] = current_offset + min(max_page_limit, int(request.GET.get(PARAM_LIMIT_SUBSTITUTEPRODUCTS, default_page_limit))) + 2
 
-    return query, params, has_group_by
+    debug_dict = dict(
+        query=query,
+        query_params=params,
+        subqueries=subquery_strings,
+        subquery_intermediate_representations=info_dicts
+    )
+
+    return query, params, has_group_by, debug_dict
 
 
 def get_product_id(kwargs):
@@ -185,35 +212,41 @@ def get_product_id(kwargs):
 @decorators.api_view(['GET', 'POST'])
 def product_list(request):
     if request.method == 'POST':
-        product_id_groups = _product_list_query(request)
+        product_id_groups, max_group_size, debug_dict = _product_list_query(request)
     else:
         product_ids = models.Product.objects.values_list('id', flat=True)
-        product_id_groups = [(i, []) for i in product_ids]
+        product_id_groups, max_group_size, debug_dict = [(i, []) for i in product_ids], 1, {}
     paginator = pagination.ProductsPagination()  # works on lists too
-    results_page = paginator.paginate_queryset(product_id_groups, request)
+    results_page = paginator.paginate_queryset(product_id_groups, request, max_count_SubstituteProducts=max_group_size - 1)
     results = OrderedDict()
     results.update(serializers.serialize_product_id_groups(results_page))
-    return paginator.get_paginated_response(results.values())
+    return paginator.get_paginated_response(results.values(), debug_dict=debug_dict)
 
 
 def _product_list_query(request):
     prod_types = obit.get_schema_subclasses('Product')
-    query, params, has_group_by = build_query(prod_types, request)
+    query, params, has_group_by, debug_dict = build_query(prod_types, request)
+    if query is None:
+        return [], 1, debug_dict
     product_groups = OrderedDict()
     with db.connection.cursor() as cursor:
         cursor.execute(query, params=params)
         # min(id) on empty table returns null
+        max_group_size = 1
         for row in cursor.fetchall():
             if has_group_by:
-                product_id, first_id = row
+                product_id, first_id, group_size = row
                 if first_id not in product_groups:
                     product_groups[first_id] = []
                 if product_id != first_id:
                     product_groups[first_id].append(product_id)
+                if max_group_size < group_size:
+                    max_group_size = group_size
             else:
                 product_id, = row  # unpack length one tuple
                 product_groups[product_id] = []
-        return list(product_groups.items())
+
+        return list(product_groups.items()), max_group_size, debug_dict
 
 
 @decorators.api_view(['GET'])
