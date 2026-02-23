d53d66a9866eb698eb89a72346a01be2f3000ca6
Taos Transue <spacechips@rocketmail.com>
2026-02-22 22:54:43 -0700

    change url namespace

--- server/urls.py
+++ server/urls.py
@@ -3,13 +3,10 @@ from django.urls import path
 from . import views
 
 
-# app_name = 'product'
-app_name = 'webpage'
+app_name = 'product'
 urlpatterns = [
-    # path('', views.product_list, name='list'),
-    # path('<uuid:ProdID_Value>', views.product_detail_by_ProdID, name='detail'),
-    path('', views.product_list, name='product-list'),
-    path('<uuid:ProdID_Value>/', views.product_detail_by_ProdID, name='product-detail-prodid'),
-    path('<uuid:ProdID_Value>/json', views.product_json, name='product-json'),
-    # path('product/<slug:ProdCode_Value>/', views.product_detail, name='product-detail-prodcode'),
+    path('', views.product_list, name='list'),
+    path('<uuid:ProdID_Value>/', views.product_detail_by_ProdID, name='detail-prodid'),
+    path('<uuid:ProdID_Value>/json', views.product_json, name='json'),
+    path('product/<slug:ProdCode_Value>/', views.product_detail_by_ProdCode, name='detail-prodcode'),
 ]
