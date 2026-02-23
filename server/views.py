00f69e2a717c7d193a2412c013a7491e3ed90782
Taos Transue <spacechips@rocketmail.com>
2026-02-22 18:33:26 -0700

    views with templates

--- server/views.py
+++ server/views.py
@@ -1,11 +1,13 @@
 import ast
 from collections import defaultdict
 import datetime
+from functools import partial
 import itertools
 
 from django.core import paginator
 import django.db.models
 from django.db.models import Q
+import django.template
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, reverse
 
@@ -14,84 +16,90 @@ import server.models as models
 import server.forms as forms
 
 
-CURRENTLY_IMPLEMENTED_PRODUCTS = ('prodbattery', 'prodmodule')
+GROUP_NAMES = ('elements', 'nested_objects', 'element_arrays', 'object_arrays')
+CURRENTLY_IMPLEMENTED_PRODUCTS = ('prodbattery', 'prodcell', 'prodmodule')
 
 
-def model_to_ob_json(model):
-    ob_json = defaultdict(dict)
+def model_to_ob_json(model, group=False):
+    ob_json_grouped = {k: defaultdict(dict) for k in GROUP_NAMES}
     ob_model = ob_models.OBObject.objects.get(name=model._meta.object_name)
     for element in ob_model.all_elements():
         if element.item_type.units.exists():
-            ob_json[element.name]['Unit'] = getattr(model, f'{element.name}_Unit')
-        ob_json[element.name]['Value'] = getattr(model, f'{element.name}_Value')
+            ob_json_grouped['elements'][element.name]['Unit'] = getattr(model, f'{element.name}_Unit')
+        ob_json_grouped['elements'][element.name]['Value'] = getattr(model, f'{element.name}_Value')
     for nested_object in ob_model.all_nested_objects():
         if getattr(model, nested_object.name) is not None:
-            ob_json[nested_object.name] = model_to_ob_json(getattr(model, nested_object.name))
+            ob_json_grouped['nested_objects'][nested_object.name] = model_to_ob_json(getattr(model, nested_object.name), group=group)
         else:
-            ob_json[nested_object.name] = None
+            ob_json_grouped['nested_objects'][nested_object.name] = None
     for element_array in ob_model.all_element_arrays():
-        ob_json[element_array.name] = []
+        ob_json_grouped['element_arrays'][element_array.name] = []
         for v in getattr(model, element_array.name).all():
             item_json = {}
             if element_array.items.item_type.units.exists():
                 item_json['Unit'] = getattr(v, 'Unit')
             item_json['Value'] = getattr(v, 'Value')
-            ob_json[element_array.name].append(item_json)
+            ob_json_grouped['element_arrays'][element_array.name].append(item_json)
     for object_array in ob_model.all_object_arrays():
         if object_array.name == 'SubstituteProducts':
-            ob_json[object_array.name] = []
+            ob_json_grouped['object_arrays'][object_array.name] = []
         else:
-            ob_json[object_array.name] = [
-                model_to_ob_json(v)
+            ob_json_grouped['object_arrays'][object_array.name] = [
+                model_to_ob_json(v, group=group)
                 for v in getattr(model, object_array.name).all()
             ]
-    return ob_json
+    ob_json_grouped = {
+        group_name: dict(group) for group_name, group in ob_json_grouped.items()
+    }
+    if group:
+        return ob_json_grouped
+    return dict(itertools.chain.from_iterable((group.items() for group in ob_json_grouped.values())))
 
 
-def get_form_dict(ob_model, d, parent_name=''):
-    name = ob_model.name
-    form_dict = dict(
-        name=name,
-        form=None,
-        object_form_dicts=[],
-        array_form_dicts=[]
-    )
-    form_initial = {}
-    form_prefix = name.lower() if parent_name == '' else f'{parent_name}-{name.lower()}'
-    for element in ob_model.all_elements():
-        match element.taxonomy_element.name:
-            case 'TaxonomyElementString':
-                default = dict(Value='')
-            case _:
-                default = dict(Value=None)
-                if element.item_type.units.exists():
-                    default['Unit'] = ''
-        form_initial[element.name] = d.get(element.name, default)
-    for nested_object in ob_model.all_nested_objects():
-        if d[nested_object.name] is not None:
-            form_dict['object_form_dicts'].append(get_form_dict(nested_object, d[nested_object.name], parent_name=form_prefix))
-    for element_array in ob_model.all_element_arrays():
-            prefix_plural = f'{form_prefix}-{element_array.name.lower()}'
-            form_dict['array_form_dicts'].append(dict(
-                plural=element_array.name,
-                prefix_plural=prefix_plural,
-                object_form_dicts=[
-                    get_form_dict(element_array.items, o, parent_name=f'{prefix_plural}-{i}')
-                    for i, o in enumerate(d[element_array.name])
-                ]
-            ))
-    for object_array in ob_model.all_object_arrays():
-        prefix_plural = f'{form_prefix}-{object_array.name.lower()}'
-        form_dict['array_form_dicts'].append(dict(
-            plural=object_array.name,
-            prefix_plural=prefix_plural,
-            object_form_dicts=[
-                get_form_dict(object_array.items, o, parent_name=f'{prefix_plural}-{i}')
-                for i, o in enumerate(d[object_array.name])
-            ]
-        ))
-    form_dict['form'] = getattr(forms, name)(initial=form_initial, prefix=form_prefix)
-    return form_dict
+# def get_form_dict(ob_model, d, parent_name=''):
+#     name = ob_model.name
+#     form_dict = dict(
+#         name=name,
+#         form=None,
+#         object_form_dicts=[],
+#         array_form_dicts=[]
+#     )
+#     form_initial = {}
+#     form_prefix = name.lower() if parent_name == '' else f'{parent_name}-{name.lower()}'
+#     for element in ob_model.all_elements():
+#         match element.taxonomy_element.name:
+#             case 'TaxonomyElementString':
+#                 default = dict(Value='')
+#             case _:
+#                 default = dict(Value=None)
+#                 if element.item_type.units.exists():
+#                     default['Unit'] = ''
+#         form_initial[element.name] = d.get(element.name, default)
+#     for nested_object in ob_model.all_nested_objects():
+#         if d[nested_object.name] is not None:
+#             form_dict['object_form_dicts'].append(get_form_dict(nested_object, d[nested_object.name], parent_name=form_prefix))
+#     for element_array in ob_model.all_element_arrays():
+#             prefix_plural = f'{form_prefix}-{element_array.name.lower()}'
+#             form_dict['array_form_dicts'].append(dict(
+#                 plural=element_array.name,
+#                 prefix_plural=prefix_plural,
+#                 object_form_dicts=[
+#                     get_form_dict(element_array.items, o, parent_name=f'{prefix_plural}-{i}')
+#                     for i, o in enumerate(d[element_array.name])
+#                 ]
+#             ))
+#     for object_array in ob_model.all_object_arrays():
+#         prefix_plural = f'{form_prefix}-{object_array.name.lower()}'
+#         form_dict['array_form_dicts'].append(dict(
+#             plural=object_array.name,
+#             prefix_plural=prefix_plural,
+#             object_form_dicts=[
+#                 get_form_dict(object_array.items, o, parent_name=f'{prefix_plural}-{i}')
+#                 for i, o in enumerate(d[object_array.name])
+#             ]
+#         ))
+#     form_dict['form'] = getattr(forms, name)(initial=form_initial, prefix=form_prefix)
+#     return form_dict
 
 
 def determine_product_subclass(product, subclass_reverse_names=None):
@@ -106,6 +114,19 @@ def determine_product_subclass(product, subclass_reverse_names=None):
             pass
 
 
+def _obobjectsubset(group_name, obobject, key_csv):
+    group = {k: obobject[group_name][k] for k in key_csv.split(',')}
+    return {group_name: group}
+
+
+for group_name in GROUP_NAMES:
+    django.template.defaulttags.register.filter(f'obobject_{group_name}_subset', partial(_obobjectsubset, group_name))
+
+# @django.template.defaulttags.register.filter
+# def obobjectelementsubset(obobject, key_csv):
+#     return _obobjectsubset(obobject, 'elements', key_csv)
+#
+#
 def product_detail_by_ProdID(request, ProdID_Value):
     # subclass_reverse_names = map(str.lower, ob_models.OBObject.filter(comprises__name='Product').values_list('name', flat=True))
     subclass_reverse_names = CURRENTLY_IMPLEMENTED_PRODUCTS
@@ -115,16 +136,16 @@ def product_detail_by_ProdID(request, ProdID_Value):
     )
     product = determine_product_subclass(product, subclass_reverse_names=subclass_reverse_names)
     name = type(product).__name__
-    product_serialized = model_to_ob_json(product)
+    product_serialized = model_to_ob_json(product, group=True)
     print(product_serialized)
     ob_model = ob_models.OBObject.objects.get(name=name)
-    form_dict = get_form_dict(ob_model, product_serialized)
+    # form_dict = get_form_dict(ob_model, product_serialized)
     return render(
         request,
-        'server/forms/product.html',
+        'server/product_detail.html',
         context=dict(
             product=product_serialized,
-            form_dict=form_dict
+            # form_dict=form_dict
         )
     )
 
